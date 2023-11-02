8c2ef77bd84e72a25c3dd66a3abbd37981e8c126
Borislav Dostov <borislav.dostov@kongsbergdigital.com>
2023-11-02 15:12:50 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+  import CreatePollForm from "./components/CreatePollForm.svelte";
+  import PollList from "./components/PollList.svelte";
+
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
+
+  let polls = [
+    {
+      id: 1,
+      question: "Python or JavaScript?",
+      answerA: "Python",
+      answerB: "JavaScript",
+      votesA: 9,
+      votesB: 15,
+    },
+  ];
+
+  const handleAdd = (e) => {
+    const poll = e.detail;
+    polls = [poll, ...polls];
+    activeItem = "Current Polls";
+  };
+</script>
+
+<Header />
+<main>
+  <Tabs {activeItem} {items} on:tabChange={tabChange} />
+  {#if activeItem === "Current Polls"}
+    <PollList {polls} />
+  {:else if activeItem === "Add New Poll"}
+    <CreatePollForm on:add={handleAdd} />
+  {/if}
+</main>
+<Footer />
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let activeItem;
+  export let items;
+</script>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <div class:active={item === activeItem}>
+        <li on:click={() => dispatch("tabChange", item)}>{item}</li>
+      </div>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .tabs {
+    margin-bottom: 40px;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  li {
+    margin: 0 16px;
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+  }
+
+  .active {
+    color: #d91b42;
+    border-bottom: 2px solid #d91b42;
+    padding-bottom: 8px;
+  }
+</style>
